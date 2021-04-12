11bbcc5e6635a7fb950774482ccd218d4e5070b4
camdenorrb <5360339+camdenorrb@users.noreply.github.com>
2021-04-12 00:52:12 -0500

    Lots of cleanups, hopefully didn't break anything :)

--- Paper-MojangAPI/src/main/java/com/destroystokyo/paper/event/brigadier/AsyncPlayerSendSuggestionsEvent.java
+++ Paper-MojangAPI/src/main/java/com/destroystokyo/paper/event/brigadier/AsyncPlayerSendSuggestionsEvent.java
@@ -4,7 +4,6 @@ import com.mojang.brigadier.suggestion.Suggestions;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
-import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.player.PlayerEvent;
 import org.jetbrains.annotations.NotNull;
